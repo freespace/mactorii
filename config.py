d39f807586a9449894ea5dd68612ed35218e0ad2
steve <steve@10135adf-9ddc-41c3-8e0e-ecca220aee7b>
2007-09-23 13:17:12 +0000

    Fixed config's small error wrt to weight
    git-svn-id: file:///var/svn-repos/mactorii@10 10135adf-9ddc-41c3-8e0e-ecca220aee7b

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ Copyright (c) 2007 . All rights reserved.
 import sys
 import os
 
-taps = 64
-weights = (1,0.5, 0.75)
+taps = 96
+weights = (1, 0.75, 0.5)
 img_size = (256, 256)
 crop_size= 100
 tmp_file=".89885e5234e2f0ae1e2015642425d1eb.jpg"
