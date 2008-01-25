a9c74aa3551636159ba8a1d0554b472d78c34a34
steve <steve@10135adf-9ddc-41c3-8e0e-ecca220aee7b>
2008-01-25 13:14:58 +0000

    new icons and fixed up the bundle build script
    git-svn-id: file:///var/svn-repos/mactorii@55 10135adf-9ddc-41c3-8e0e-ecca220aee7b

--- build_bundle.sh
+++ build_bundle.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+echo "removing old directories"
 rm -rf build/*
 rm -rf dist/*
 
+echo "running py2ap"
+
 python setup.py py2app
 cd dist
+
+echo "producing zip"
 zip -r -9 mactorii.app.zip mactorii.app
 cd ../
+
+echo "done"
