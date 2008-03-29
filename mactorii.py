bc82a32d54833bf7de9fe9bc11b8e2f3fff156ed
Admin <freesapce@gmail.com>
2008-03-29 15:37:08 +1100

    clean up

--- mactorii.py
+++ mactorii.py
@@ -28,7 +28,7 @@ from pyglet.window import key
 from pyglet.window import mouse
 from pyglet import font
 
-version=0.76
+version="$Rev$".split(' ')[1]
 
 class MactoriiApplication:
 	renderables_key_func = lambda x : 0
